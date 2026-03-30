c21b7807ba83c66f7008c70993da76dcc3334d89
CS9490 <cristianstatescu.cs9490@gmail.com>
2026-03-30 16:40:15 -0400

    added fct-purpleair_sensors_hourly.sql

---
+++ dbt/air_quality_dbt/models/fct_purpleair_sensors_hourly.sql
@@ -0,0 +1,16 @@
+{{
+  config(
+    materialized='table',
+    partition_by={
+      'field': 'pulled_date',
+      'data_type': 'date'
+    },
+    cluster_by=['sensor_index']
+  )
+}}
+
+select
+  *,
+  date(pulled_hour) as pulled_date
+from {{ ref('int_purpleair_sensors_ts_deduped') }}
+where pulled_hour is not null
