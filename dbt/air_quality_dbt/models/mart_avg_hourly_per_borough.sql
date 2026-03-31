57842b4ff30b732230c6f277840bb7e1075d9077
CS9490 <cristianstatescu.cs9490@gmail.com>
2026-03-30 23:46:27 -0400

    fixed column name to pm10_0_atm

--- dbt/air_quality_dbt/models/mart_avg_hourly_per_borough.sql
+++ dbt/air_quality_dbt/models/mart_avg_hourly_per_borough.sql
@@ -15,7 +15,7 @@ select
   date(pulled_hour) as pulled_date,
   avg(pm1_0_atm) as avg_pm1_0_atm,
   avg(pm2_5_atm) as avg_pm2_5_atm,
-  avg(pm10_atm) as avg_pm10_atm
+  avg(pm10_0_atm) as avg_pm10_0_atm
 from {{ ref('int_purpleair_sensors_hourly_with_borough') }}
 where boro_name is not null
 group by boro_name, pulled_hour, pulled_date
