d97811fcd5ed59b226d5b4ca93e1c285b2d91bae
CS9490 <cristianstatescu.cs9490@gmail.com>
2026-03-30 23:38:57 -0400

    mart_avg_hourly_per_borough.sql - made table for dashboard

--- dbt/air_quality_dbt/models/mart_avg_hourly_per_borough.sql
+++ dbt/air_quality_dbt/models/mart_avg_hourly_per_borough.sql
@@ -2,9 +2,20 @@
   config(
     materialized='table',
     partition_by={
-      'field': 'boro_name',
-      'data_type': 'string'
+      'field': 'pulled_date',
+      'data_type': 'date'
     },
-    cluster_by=['sensor_index']
+    cluster_by=['boro_name']
   )
 }}
+
+select
+  boro_name,
+  pulled_hour,
+  date(pulled_hour) as pulled_date,
+  avg(pm1_0_atm) as avg_pm1_0_atm,
+  avg(pm2_5_atm) as avg_pm2_5_atm,
+  avg(pm10_atm) as avg_pm10_atm
+from {{ ref('int_purpleair_sensors_hourly_with_borough') }}
+where boro_name is not null
+group by boro_name, pulled_hour, pulled_date
