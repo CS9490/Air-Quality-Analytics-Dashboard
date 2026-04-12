a5b55bded7bc35b22a7df4bf28c2ad46165e6d5c
CS9490 <cristianstatescu.cs9490@gmail.com>
2026-04-12 15:52:50 -0400

    changed up dbt models and also made project use dbt through uv

--- dbt/air_quality_dbt/models/int_sensors_hourly_with_borough.sql
+++ dbt/air_quality_dbt/models/int_sensors_hourly_with_borough.sql
@@ -3,7 +3,8 @@
 select
   s.*,
   b.boro_code,
-  b.boro_name
+  b.boro_name,
+  b.boro_geog
 from {{ ref('int_purpleair_sensors_ts_mapped_deduped') }} s
 left join {{ ref('dim_nyc_boroughs') }} b
   on st_within(s.location_point, b.boro_geog)
--- dbt/air_quality_dbt/models/mart_avg_hourly_per_borough.sql
+++ dbt/air_quality_dbt/models/mart_avg_hourly_per_borough.sql
@@ -15,7 +15,8 @@ select
   date(pulled_hour) as pulled_date,
   avg(pm1_0_atm) as avg_pm1_0_atm,
   avg(pm2_5_atm) as avg_pm2_5_atm,
-  avg(pm10_0_atm) as avg_pm10_0_atm
-from {{ ref('int_purpleair_sensors_hourly_with_borough') }}
+  avg(pm10_0_atm) as avg_pm10_0_atm,
+  boro_geog
+from {{ ref('int_sensors_hourly_with_borough') }}
 where boro_name is not null
 group by boro_name, pulled_hour, pulled_date
