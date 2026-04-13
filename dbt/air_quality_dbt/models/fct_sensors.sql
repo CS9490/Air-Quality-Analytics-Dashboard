915a6d7f67fc22db189bd0d9e2b879c24a225dfa
CS9490 <cristianstatescu.cs9490@gmail.com>
2026-04-13 00:53:47 -0400

    removed unneeded fct table and make new fct_sensors table to pull in lat and long on aggregated mart mart_sensors_hourly_data_mapped for heatmap in dashboard

---
+++ dbt/air_quality_dbt/models/fct_sensors.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='table') }}
+
+select
+    distinct(sensor_index),
+    latitude,
+    longitude,
+    boro_name
+  from {{ ref('int_sensors_hourly_with_borough') }}
---
+++ dbt/air_quality_dbt/models/mart_sensors_hourly_data_mapped.sql
@@ -0,0 +1,32 @@
+ {{
+  config(
+    materialized='table',
+    partition_by={
+      'field': 'pulled_date',
+      'data_type': 'date'
+    },
+    cluster_by=['sensor_index']
+  )
+}}
+
+with cte as (
+  select
+  sensor_index,
+  avg(pm1_0_atm) as avg_pm1_0_atm,
+  avg(pm2_5_atm) as avg_pm2_5_atm,
+  avg(pm10_0_atm) as avg_pm10_0_atm,
+  datetime(pulled_hour, 'America/New_York') as pulled_hour_est,
+  date(pulled_hour, 'America/New_York') as pulled_date
+from {{ ref('int_sensors_hourly_with_borough') }}
+where boro_name is not null
+group by sensor_index, pulled_hour_est, pulled_date
+)
+
+select cte.*, 
+  st_geogpoint(
+    safe_cast(fct_sensors.longitude as float64),
+    safe_cast(fct_sensors.latitude as float64)
+  ) as location_point
+from cte
+left join {{ ref('fct_sensors') }} fct_sensors
+on cte.sensor_index = fct_sensors.sensor_index
