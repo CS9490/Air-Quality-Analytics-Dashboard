64bc61d46fff06f37155f8906291b1a7883f6c8c
CS9490 <cristianstatescu.cs9490@gmail.com>
2026-04-17 22:37:31 -0400

    removed fct_sensors.sql (unneeded) and edited mart_best_time_to_be_out.sql

--- dbt/air_quality_dbt/models/mart_best_time_to_be_out.sql
+++ dbt/air_quality_dbt/models/mart_best_time_to_be_out.sql
@@ -9,23 +9,11 @@
   )
 }}
 
-with cte as (
-  select
+select
   boro_name,
   avg(pm2_5_atm) as avg_pm2_5_atm,
   datetime(pulled_hour, 'America/New_York') as pulled_hour_est,
   date(pulled_hour, 'America/New_York') as pulled_date
 from {{ ref('int_sensors_hourly_with_borough') }}
-where boro_name is not null
-group by boro_name, pulled_hour_est, pulled_date
-)
-
--- used to map sensors with heatmap style map, but not added to dashboard, so commented out for now
-select cte.* 
-  -- ,st_geogpoint(
-  --   safe_cast(fct_sensors.longitude as float64),
-  --   safe_cast(fct_sensors.latitude as float64)
-  -- ) as location_point
-from cte
--- left join {{ ref('fct_sensors') }} fct_sensors
--- on cte.sensor_index = fct_sensors.sensor_index
+  where boro_name is not null
+  group by boro_name, pulled_hour_est, pulled_date
